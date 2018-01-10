b54432476b5a6657a691f45f269973487e48e723
Peter Kong <bansuki@gmail.com>
2018-01-09 22:48:50 -0600

    scraping content successfully - still tweaking formatting

--- teks-codes/scrape_teks.py
+++ teks-codes/scrape_teks.py
@@ -1,10 +1,45 @@
-from scrapely import Scraper
-s = Scraper()
+from urllib.request import urlopen
+import re
+import json
 
-# TODO iterate through list of chapters - i.e. subjects
-# TODO iterate through list of subchapters - i.e. education level
-# TODO grab knowledge and skills section using scrapely
-# TODO parse subsections
-# TODO concat location of clause to make key
-# TODO hash key with subsection text concat with clause text
-# TODO save hash to json file
+url = "http://ritter.tea.state.tx.us/rules/tac/chapter113/ch113c.html"
+html = str(urlopen(url).read())
+in_correct_section = False
+in_correct_subsection = False
+current_paragraph = {}
+teks_dict = {}
+for paragraph in html.split('<p'):
+    text = re.search(r'(?<=>).+(?=<)', paragraph).group()
+
+    if not in_correct_section:
+        if re.search('United States Government', text):
+            in_correct_section = True
+    elif not in_correct_subsection:
+        if re.search('Knowledge and skills', text):
+            in_correct_subsection = True
+    else:
+        m = re.search(r'(?<=class=)\w+(?=(>))', paragraph)
+        if m:
+            klass = m.group()
+            if klass == 'SOURCENOTE':
+                break
+            code, text = text.split('&nbsp;&nbsp;')
+            text = text.replace(' \\r\\n ', '')
+            code = code[1:-1]
+            if klass == 'PARAGRAPH1':
+                l = text.split('. ')
+                subject_code = l[0][:3].lower()
+                text = '. '.join(l[1:])
+
+                current_paragraph = {
+                    'code': code,
+                    'text': text,
+                    'subject_code': subject_code
+                }
+            elif klass == 'SUBPARAGRAPHA':
+
+                key = subject_code + current_paragraph['code'] + code
+                teks_dict[key] = current_paragraph['text'] + text
+
+with open('teks_dict.json', 'w') as teks_file:
+    json.dump(teks_dict, teks_file)
