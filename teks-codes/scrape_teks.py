52076686f1a66fe0b7733cdf5db24c3c072471b5
Peter Kong <bansuki@gmail.com>
2018-01-09 20:13:01 -0600

    created initial pseudocode scraper script

---
+++ teks-codes/scrape_teks.py
@@ -0,0 +1,10 @@
+from scrapely import Scraper
+s = Scraper()
+
+# TODO iterate through list of chapters - i.e. subjects
+# TODO iterate through list of subchapters - i.e. education level
+# TODO grab knowledge and skills section using scrapely
+# TODO parse subsections
+# TODO concat location of clause to make key
+# TODO hash key with subsection text concat with clause text
+# TODO save hash to json file
